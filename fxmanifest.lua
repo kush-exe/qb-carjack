526734b84fa8289b583226a0fb369aaff925b4ad
Kush <kooshpatel@gmail.com>
2022-06-10 15:00:39 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+games {'gta5'}
+
+dependencies {
+    "PolyZone"
+}
+
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
+    'client/client.lua'
+}
+
+
